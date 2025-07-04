280c4cf0eaafa955bcc6046d32214aa18910f488
Draok <mickael.malatesta@laposte.net>
2025-07-04 21:59:23 +0200

    Load DoctrineFixturesBundle in production to enable fixtures load on Heroku

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
