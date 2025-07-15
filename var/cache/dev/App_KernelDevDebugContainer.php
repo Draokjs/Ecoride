602d87f14ff794b5009c49f00e5d0a76d4fce355
Draok <mickael.malatesta@laposte.net>
2025-07-15 20:50:34 +0200

    Fix register form width

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerZmhN3ao\App_KernelDevDebugContainer([
     'container.build_hash' => 'ZmhN3ao',
-    'container.build_id' => 'fcf9489e',
-    'container.build_time' => 1752112742,
+    'container.build_id' => '51df9628',
+    'container.build_time' => 1752605263,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerZmhN3ao');
