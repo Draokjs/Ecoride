a43638043e896e1e2b6fe173ecafb28720ba4d1e
Draok <mickael.malatesta@laposte.net>
2025-07-15 22:38:39 +0200

    Removed register form container to define css customized

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerZmhN3ao\App_KernelDevDebugContainer([
     'container.build_hash' => 'ZmhN3ao',
-    'container.build_id' => '51df9628',
-    'container.build_time' => 1752605263,
+    'container.build_id' => 'db7ae95b',
+    'container.build_time' => 1752611622,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerZmhN3ao');
