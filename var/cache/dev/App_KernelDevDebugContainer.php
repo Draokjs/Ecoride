d68583ff82499569dd804e7df58b1660379ec1bd
Draok <mickael.malatesta@laposte.net>
2025-07-17 03:32:22 +0200

    register changed applied to main branches

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerZmhN3ao\App_KernelDevDebugContainer([
     'container.build_hash' => 'ZmhN3ao',
-    'container.build_id' => '8113e3e9',
-    'container.build_time' => 1752712135,
+    'container.build_id' => '66745918',
+    'container.build_time' => 1752715883,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerZmhN3ao');
