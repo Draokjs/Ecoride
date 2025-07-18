2a1676085af8f7821d6653b6d99a633e35bf82fc
Draok <mickael.malatesta@laposte.net>
2025-07-18 09:20:29 +0200

    fix continue

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
+    #[Route('/login', name: 'login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // Get login error if there is one
